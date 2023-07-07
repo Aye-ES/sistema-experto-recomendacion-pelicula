273db630a56101a69b238e3e542c5e70c396e1d6
Aye-ES <ayelenestevezs@gmail.com>
2023-07-07 11:49:08 -1000

    agregando sistema experto python+documentacion

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 import sqlite3
 from pyknow import *
 
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+
 # Definición de clases
 class Usuario(Fact):
     pass
@@ -80,5 +85,53 @@ conn.commit()
 conn.close()
 
 
+# Definir la estructura de la red bayesiana
+
+# Crear el objeto de la red bayesiana
+modelo_bayesiano = BayesianModel()
+
+# Definir las variables
+variables = ["Usuario", "Genero", "Calificacion", "RecomendacionPelicula"]
+
+# Agregar las variables al modelo
+modelo_bayesiano.add_nodes_from(variables)
+
+# Definir las relaciones entre las variables
+modelo_bayesiano.add_edges_from([
+    ("Usuario", "Calificacion"),
+    ("Genero", "RecomendacionPelicula"),
+    ("Calificacion", "RecomendacionPelicula")
+])
 
 
+# Definir las distribuciones de probabilidad condicional (CPDs) de las siguientes variables:
+
+# Definir CPD para la variable "Usuario"
+cpd_usuario = TabularCPD(variable="Usuario", variable_card=2, values=[[0.6, 0.4]])
+
+# Definir CPD para la variable "Genero"
+cpd_genero = TabularCPD(variable="Genero", variable_card=2, values=[[0.5, 0.5]])
+
+# Definir CPD para la variable "Calificacion"
+cpd_calificacion = TabularCPD(variable="Calificacion", variable_card=2,
+                             values=[[0.2, 0.8], [0.7, 0.3]],
+                             evidence=["Usuario"], evidence_card=[2])
+
+# Definir CPD para la variable "RecomendacionPelicula"
+cpd_recomendacion = TabularCPD(variable="RecomendacionPelicula", variable_card=2,
+                              values=[[0.3, 0.6, 0.1, 0.9], [0.7, 0.4, 0.9, 0.1]],
+                              evidence=["Genero", "Calificacion"],
+                              evidence_card=[2, 2])
+
+# Asociar los CPDs al modelo
+modelo_bayesiano.add_cpds(cpd_usuario, cpd_genero, cpd_calificacion, cpd_recomendacion)
+
+
+
+# Realizar inferencias utilizando el motor de inferencia:
+# Crear un objeto de VariableElimination para realizar inferencias
+inferencia = VariableElimination(modelo_bayesiano)
+
+# Realizar una inferencia específica
+resultado = inferencia.query(variables=["RecomendacionPelicula"], evidence={"Usuario": 1})
+print(resultado)
