cb422de12c16a28250649a3b0d02c49609daae32
Aye-ES <ayelenestevezs@gmail.com>
2023-07-14 08:44:49 -1000

    Mejoramiento de base_conocimiento y conexion_sqlite3

--- base_conocimiento.py
+++ base_conocimiento.py
@@ -1,25 +1,24 @@
 from pgmpy.models import BayesianModel
 from pgmpy.factors.discrete import TabularCPD
 
-# Definir la base de conocimiento utilizando redes bayesianas
 def create_bayesian_model():
     # Crear el objeto de la red bayesiana
     modelo_bayesiano = BayesianModel()
 
     # Definir las variables
-    variables = ["Usuario", "Genero", "Calificacion", "RecomendacionPelicula", "Clasificacion", "Pelicula"]
+    variables = ["Usuario", "RecomendacionPelicula", "Calificacion", "Pelicula", "Clasificacion", "Genero"]
 
     # Agregar las variables al modelo
     modelo_bayesiano.add_nodes_from(variables)
 
     # Definir las relaciones entre las variables
     modelo_bayesiano.add_edges_from([
+        ("Usuario", "RecomendacionPelicula"),
         ("Usuario", "Calificacion"),
-        ("Genero", "RecomendacionPelicula"),
-        ("Calificacion", "RecomendacionPelicula"),
+        ("Pelicula", "RecomendacionPelicula"),
+        ("Pelicula", "Calificacion"),
         ("Clasificacion", "Pelicula"),
-        ("Genero", "Pelicula"),
-        ("Calificacion", "Pelicula")
+        ("Genero", "Pelicula")
     ])
 
     # Definir las distribuciones de probabilidad condicional (CPDs) de las variables
@@ -27,30 +26,35 @@ def create_bayesian_model():
     # Definir CPD para la variable "Usuario"
     cpd_usuario = TabularCPD(variable="Usuario", variable_card=2, values=[[0.6, 0.4]])
 
-    # Definir CPD para la variable "Genero"
-    cpd_genero = TabularCPD(variable="Genero", variable_card=2, values=[[0.5, 0.5]])
+    # Definir CPD para la variable "RecomendacionPelicula"
+    cpd_recomendacion = TabularCPD(variable="RecomendacionPelicula", variable_card=2,
+                                   values=[[0.3, 0.6, 0.1, 0.9], [0.7, 0.4, 0.9, 0.1]],
+                                   evidence=["Usuario", "Pelicula"],
+                                   evidence_card=[2, 2])
 
     # Definir CPD para la variable "Calificacion"
     cpd_calificacion = TabularCPD(variable="Calificacion", variable_card=2,
                                   values=[[0.2, 0.8], [0.7, 0.3]],
-                                  evidence=["Usuario"], evidence_card=[2])
-
-    # Definir CPD para la variable "RecomendacionPelicula"
-    cpd_recomendacion = TabularCPD(variable="RecomendacionPelicula", variable_card=2,
-                                   values=[[0.3, 0.6, 0.1, 0.9], [0.7, 0.4, 0.9, 0.1]],
-                                   evidence=["Genero", "Calificacion"],
-                                   evidence_card=[2, 2])
-
-    # Definir CPD para la variable "Clasificacion"
-    cpd_clasificacion = TabularCPD(variable="Clasificacion", variable_card=2, values=[[0.5, 0.5]])
+                                  evidence=["Usuario", "Pelicula"],
+                                  evidence_card=[2, 2])
 
     # Definir CPD para la variable "Pelicula"
     cpd_pelicula = TabularCPD(variable="Pelicula", variable_card=2,
                               values=[[0.3, 0.7], [0.6, 0.4], [0.8, 0.2], [0.1, 0.9]],
-                              evidence=["Clasificacion", "Genero", "Calificacion"],
-                              evidence_card=[2, 2, 2])
+                              evidence=["Clasificacion", "Genero"],
+                              evidence_card=[2, 2])
+
+    # Definir CPD para la variable "Clasificacion"
+    cpd_clasificacion = TabularCPD(variable="Clasificacion", variable_card=2, values=[[0.5, 0.5]])
+
+    # Definir CPD para la variable "Genero"
+    cpd_genero = TabularCPD(variable="Genero", variable_card=2, values=[[0.5, 0.5]])
 
     # Asociar los CPDs al modelo
-    modelo_bayesiano.add_cpds(cpd_usuario, cpd_genero, cpd_calificacion, cpd_recomendacion, cpd_clasificacion, cpd_pelicula)
+    modelo_bayesiano.add_cpds(cpd_usuario, cpd_recomendacion, cpd_calificacion, cpd_pelicula, cpd_clasificacion,
+                              cpd_genero)
 
     return modelo_bayesiano
+
+# Crear la base de conocimiento utilizando el modelo bayesiano
+base_conocimiento = create_bayesian_model()
--- conexion_sqlite3.py
+++ conexion_sqlite3.py
@@ -1,4 +1,6 @@
 import sqlite3
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete import TabularCPD
 
 # Conectar a la base de datos SQLite
 def connect_to_database():
@@ -11,14 +13,13 @@ conn, cursor = connect_to_database()
 
 # Tabla Usuario
 cursor.execute('''CREATE TABLE IF NOT EXISTS Usuario (
-                    id INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nombre TEXT,
                     email TEXT)''')
 
-
 # Tabla RecomendacionPelicula
 cursor.execute('''CREATE TABLE IF NOT EXISTS RecomendacionPelicula (
-                    id INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     usuario_id INTEGER,
                     pelicula_id INTEGER,
                     FOREIGN KEY (usuario_id) REFERENCES Usuario (id),
@@ -26,7 +27,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS RecomendacionPelicula (
 
 # Tabla Calificacion
 cursor.execute('''CREATE TABLE IF NOT EXISTS Calificacion (
-                    id INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     usuario_id INTEGER,
                     pelicula_id INTEGER,
                     valor INTEGER,
@@ -35,20 +36,22 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Calificacion (
 
 # Tabla Pelicula
 cursor.execute('''CREATE TABLE IF NOT EXISTS Pelicula (
-                    id INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     titulo TEXT,
                     clasificacion_id INTEGER,
                     FOREIGN KEY (clasificacion_id) REFERENCES Clasificacion (id))''')
 
 # Tabla Clasificacion
 cursor.execute('''CREATE TABLE IF NOT EXISTS Clasificacion (
-                    id INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nombre TEXT)''')
-
-# Tabla Genero
+#Tabla Genero
 cursor.execute('''CREATE TABLE IF NOT EXISTS Genero (
-                    id INTEGER PRIMARY KEY,
-                    nombre TEXT)''')
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre TEXT,
+                    pelicula_id INTEGER,
+                    FOREIGN KEY (pelicula_id) REFERENCES Pelicula (id))''')
+
 
 # Cerrar conexión a la base de datos SQLite
 def close_connection(conn):
@@ -57,33 +60,60 @@ def close_connection(conn):
 
 
 
+# Ejemplo de recolección de datos y guardado en la base de datos
 
-# Función para crear un nuevo usuario
-def create_usuario(nombre, email):
-    # Insertar un nuevo usuario
-    cursor.execute("INSERT INTO Usuario (nombre, email) VALUES (?, ?)", (nombre, email))
-    conn.commit()
+def collect_usuario_data():
+    usuario_data = [
+        #("Leandro", "leandro@gmail.com"),
+        # Agrega más datos de usuario aquí
+    ]
+    cursor.executemany('INSERT INTO Usuario (nombre, email) VALUES (?, ?)', usuario_data)
 
+def collect_recomendacion_data():
+    recomendacion_data = [
+        # (1, 1),
+        # Agrega más datos de recomendación aquí
+    ]
+    cursor.executemany('INSERT INTO RecomendacionPelicula (usuario_id, pelicula_id) VALUES (?, ?)', recomendacion_data)
 
-# Función para eliminar un usuario
-def delete_usuario(usuario_id):
-    # Eliminar el usuario de la tabla Usuario
-    cursor.execute("DELETE FROM Usuario WHERE id = ?", (usuario_id,))
-    conn.commit()
-    
-    
-    
-    
-    
-# Ejemplo de creación de usuario
-nombre = "Susana Estevez Segovia"
-email = "susana@gmail.com"
-create_usuario(nombre, email)
+def collect_calificacion_data():
+    calificacion_data = [
+        # (1, 1, 5),
+        # Agrega más datos de calificación aquí
+    ]
+    cursor.executemany('INSERT INTO Calificacion (usuario_id, pelicula_id, valor) VALUES (?, ?, ?)', calificacion_data)
 
+def collect_pelicula_data():
+    pelicula_data = [
+        # ("Pelicula1", 1),
+        # Agrega más datos de película aquí
+    ]
+    cursor.executemany('INSERT INTO Pelicula (titulo, clasificacion_id) VALUES (?, ?)', pelicula_data)
 
-# Ejemplo de eliminación de usuario
-#usuario_id = 6
-#delete_usuario(usuario_id)
+def collect_clasificacion_data():
+    clasificacion_data = [
+        # ("Clasificacion1"),
+        # Agrega más datos de clasificación aquí
+    ]
+    cursor.executemany('INSERT INTO Clasificacion (nombre) VALUES (?)', clasificacion_data)
+
+def collect_genero_data():
+    genero_data = [
+        # ("Genero1", 1),
+        # Agrega más datos de género aquí
+    ]
+    cursor.executemany('INSERT INTO Genero (nombre, pelicula_id) VALUES (?, ?)', genero_data)
+
+# Ejecutar las funciones de recolección de datos
+collect_usuario_data()
+collect_recomendacion_data()
+collect_calificacion_data()
+collect_pelicula_data()
+collect_clasificacion_data()
+collect_genero_data()
+
+# Guardar los cambios en la base de datos
+conn.commit()
 
 # Cerrar la conexión a la base de datos
 close_connection(conn)
