f7ccf4a807dd4be7b71157a7332b761a91485295
Aye-ES <ayelenestevezs@gmail.com>
2023-07-14 18:15:12 -1000

    cargado de datos en database y algo de implementacion interfazgrafica

--- interfaz_grafica.py
+++ interfaz_grafica.py
@@ -1,29 +1,94 @@
-from tkinter import Tk, Button
+import tkinter as tk
 
-from base_conocimiento import create_bayesian_model
-from motor_inferencia import perform_inference
-from conexion_sqlite3 import connect_to_database, close_connection
+# Función para iniciar sesión
+def login():
+    # Obtener los valores de correo electrónico y contraseña ingresados por el usuario
+    email = email_entry.get()
+    password = password_entry.get()
 
-# Crear instancia de la base de conocimiento utilizando redes bayesianas
-modelo_bayesiano = create_bayesian_model()
+    # Realizar la lógica de autenticación y validación de datos
+    # ...
 
-# Crear ventana
-ventana = Tk()
+    # Cerrar la ventana de inicio de sesión después de iniciar sesión exitosamente
+    login_window.destroy()
 
-# Función para mostrar las recomendaciones
-def mostrar_recomendaciones():
-    # Conectar a la base de datos
-    conn, cursor = connect_to_database()
+# Ventana de inicio de sesión
+def show_login_window():
+    login_window = tk.Tk()
 
-    # Realizar inferencia y mostrar recomendaciones
-    perform_inference(modelo_bayesiano)
+    # Configurar la ventana de inicio de sesión
+    login_window.title("Inicio de sesión")
+    login_window.configure(bg="lightgray")  # Establecer el color de fondo
 
-    # Cerrar conexión a la base de datos
-    close_connection(conn)
+    # Obtener el tamaño de la pantalla
+    screen_width = login_window.winfo_screenwidth()
+    screen_height = login_window.winfo_screenheight()
 
-# Botón para obtener recomendaciones
-btn_recomendaciones = Button(ventana, text="Obtener Recomendaciones", command=mostrar_recomendaciones)
-btn_recomendaciones.pack()
+    # Calcular las coordenadas para centrar la ventana en la pantalla
+    window_width = 400
+    window_height = 300
+    x = (screen_width // 2) - (window_width // 2)
+    y = (screen_height // 2) - (window_height // 2)
 
-# Ejecutar ventana
-ventana.mainloop()
+    # Establecer la geometría de la ventana
+    login_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+    # Crear etiqueta y campo de entrada para correo electrónico
+    email_label = tk.Label(login_window, text="Correo electrónico:")
+    email_label.place(relx=0.5, rely=0.35, anchor="center")
+    email_entry = tk.Entry(login_window)
+    email_entry.place(relx=0.5, rely=0.45, anchor="center")
+
+    # Crear etiqueta y campo de entrada para contraseña
+    password_label = tk.Label(login_window, text="Contraseña:")
+    password_label.place(relx=0.5, rely=0.55, anchor="center")
+    password_entry = tk.Entry(login_window, show="*")
+    password_entry.place(relx=0.5, rely=0.65, anchor="center")
+
+    # Crear botón de inicio de sesión
+    login_button = tk.Button(login_window, text="Iniciar sesión", command=login)
+    login_button.place(relx=0.5, rely=0.75, anchor="center")
+
+    # Actualizar la ventana para que aparezca centrada
+    login_window.update_idletasks()
+
+    # Ejecutar el bucle principal de la ventana de inicio de sesión
+    login_window.mainloop()
+    #############################################
+
+
+
+
+
+
+
+
+
+
+    
+# Ventana de registro de usuario
+def show_register_window():
+    register_window = tk.Tk()
+    # Configurar la ventana de registro de usuario
+    # ...
+    register_window.mainloop()
+
+# Ventana principal
+def show_main_window():
+    main_window = tk.Tk()
+    # Configurar la ventana principal
+    # ...
+    main_window.mainloop()
+
+
+
+
+
+# Función principal
+def main():
+    # Lógica principal del programa
+    show_login_window()
+
+# Punto de entrada del programa
+if __name__ == "__main__":
+    main()
--- motor_inferencia.py
+++ motor_inferencia.py
@@ -1,10 +1,5 @@
 from pgmpy.inference import VariableElimination
+from base_conocimiento import base_conocimiento
 
-# Realizar inferencias utilizando el motor de inferencia
-def perform_inference(modelo_bayesiano):
-    # Crear un objeto de VariableElimination para realizar inferencias
-    inferencia = VariableElimination(modelo_bayesiano)
 
-    # Realizar una inferencia específica
-    resultado = inferencia.query(variables=["RecomendacionPelicula"], evidence={"Usuario": 1})
-    print(resultado)
+inferencia = VariableElimination(base_conocimiento)
